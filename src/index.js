eb4af37e0230ce6a090771d066a83990bda43d83
Nur Azhim Ramadhan <nurazhimrr@gmail.com>
2026-01-04 21:40:20 +0700

    fix github pages routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/portofolio-ramzzhim">
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
 );
